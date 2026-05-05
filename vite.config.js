86198011f5b10b3ea36b69b2181b4ff9cf4ad2eb
sangwon-lee302 <200340413+sangwon-lee302@users.noreply.github.com>
2026-05-05 22:01:52 +0900

    fix: add logo image file in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/images/COACHTECH_logo.png',
+            ],
             refresh: true,
         }),
         tailwindcss(),
