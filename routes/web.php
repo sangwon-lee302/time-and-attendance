ff2c6b293bd810741627b4cac780bc9dd9c2cbd7
sangwon-lee302 <200340413+sangwon-lee302@users.noreply.github.com>
2026-05-29 22:14:31 +0900

    refactor: remove unnecessary route and blade

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ use App\Http\Controllers\AttendanceCorrectionApplicationController;
 use App\Http\Controllers\TimeLogController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'verified'])->get('preview', fn () => view('preview'));
-
 Route::middleware(['auth', 'verified'])->name('time-logs.')->group(function () {
     Route::get('attendance', [TimeLogController::class, 'create'])->name('create');
     Route::post('attendance/clock-in', [TimeLogController::class, 'clockIn'])->name('clock-in');
