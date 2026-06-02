daae58165c97d6b26d18d017c5f1d167121690c6
sangwon-lee302 <200340413+sangwon-lee302@users.noreply.github.com>
2026-06-02 20:01:24 +0900

    feat: implement test suite for csv export feature

--- tests/Feature/Admin/AttendanceIndexTest.php
+++ tests/Feature/Admin/AttendanceIndexTest.php
@@ -37,4 +37,64 @@ class AttendanceIndexTest extends TestCase
         // check if links for attendance show page exists
         $request->assertSee('href="'.url('admin/attendance/'.$attendance->id).'"', false);
     }
+
+    public function test_admin_user_can_export_attendances_as_csv(): void
+    {
+        $this->freezeTime();
+
+        $adminUser = User::factory()->admin()->create();
+        $user      = User::factory()->create();
+        foreach (Attendance::factory(5)->recycle($user)->create() as $attendance) {
+            BreakTime::factory()->withinAttendance($attendance)->create();
+        }
+
+        $this->actingAs($adminUser)->get(
+            'admin/attendance/staff/'.$user->id
+        )->assertOk();
+
+        $response = $this->actingAs($adminUser)->get(route('admin.export', [
+            'user'  => $user,
+            'month' => now()->format('Y-m'),
+        ]));
+
+        $response->assertOk();
+
+        $response->assertHeaderContains('Content-Type', 'text/csv');
+        $response->assertHeader(
+            'Content-Disposition',
+            'attachment; filename="attendances.csv"',
+        );
+
+        $csvContent = $response->streamedContent();
+
+        $this->assertStringContainsString("\xEF\xBB\xBF", $csvContent);
+        $this->assertStringContainsString('日付,出勤,退勤,休憩,合計', $csvContent);
+        $attendances = Attendance::whereDate('date', today())
+            ->with('breakTimes', fn ($query) => $query->whereNotNull('ended_at')
+                ->select('id', 'attendance_id', 'started_at', 'ended_at')
+            )
+            ->get();
+        foreach ($attendances as $attendance) {
+            $this->assertStringContainsString(
+                $attendance->date->isoFormat('MM/DD(ddd)'),
+                $csvContent,
+            );
+            $this->assertStringContainsString(
+                $attendance->clocked_in_at->format('H:i'),
+                $csvContent,
+            );
+            $this->assertStringContainsString(
+                $attendance->clocked_out_at->format('H:i'),
+                $csvContent,
+            );
+            $this->assertStringContainsString(
+                $attendance->total_break_time->format('%h:%I'),
+                $csvContent,
+            );
+            $this->assertStringContainsString(
+                $attendance->total_working_time->format('%h:%I'),
+                $csvContent,
+            );
+        }
+    }
 }
