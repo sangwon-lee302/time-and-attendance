d62953f8bd5ccdca423d13028b6eb6a4797dd57f
sangwon-lee302 <200340413+sangwon-lee302@users.noreply.github.com>
2026-06-07 15:44:18 +0900

    chore: stage some change

--- routes/web.php
+++ routes/web.php
@@ -2,77 +2,75 @@
 
 use App\Http\Controllers\Admin\AttendanceController as AdminAttendanceController;
 use App\Http\Controllers\Admin\AuthenticatedSessionController;
-use App\Http\Controllers\Admin\CorrectionApplicationController as AdminCorrectionApplicationController;
+use App\Http\Controllers\Admin\StampCorrectionController as AdminStampCorrectionController;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\StampCorrectionController;
 use App\Http\Controllers\TimeLogController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::prefix('attendance')->group(function () {
-        Route::get('/', [TimeLogController::class, 'create'])->name('time-logs.create');
-        Route::post('clock-in', [TimeLogController::class, 'clockIn'])
-            ->name('time-logs.clock-in');
-        Route::patch('clock-out', [TimeLogController::class, 'clockOut'])
-            ->name('time-logs.clock-out');
-        Route::post('break-start', [TimeLogController::class, 'breakStart'])
-            ->name('time-logs.break-start');
-        Route::patch('break-end', [TimeLogController::class, 'breakEnd'])
-            ->name('time-logs.break-end');
-
-        Route::get('list', [AttendanceController::class, 'index'])
-            ->name('attendances.index');
-        Route::get('detail/{attendance}', [AttendanceController::class, 'show'])
-            ->can('view', 'attendance')
-            ->name('attendances.show');
-    });
-
-    Route::get(
-        'stamp_correction_request/list',
-        [StampCorrectionController::class, 'index']
-    )
-        ->name('stamp-corrections.index');
-    Route::post(
-        'stamp-correction/{attendance}',
-        [StampCorrectionController::class, 'store']
-    )
-        ->name('stamp-corrections.store');
+Route::middleware(['guest'])
+    ->controller(AuthenticatedSessionController::class)
+    ->prefix('admin/login')
+    ->name('admin.login')
+    ->group(function () {
+        Route::get('/', 'create');
+        Route::post('/', 'store');
 });
 
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::middleware('guest')->group(function () {
-        Route::get('login', [AuthenticatedSessionController::class, 'create'])
-            ->name('login');
-        Route::post('login', [AuthenticatedSessionController::class, 'store'])
-            ->name('login');
-    });
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::controller(TimeLogController::class)
+        ->prefix('attendance')
+        ->name('time-logs.')
+        ->group(function () {
+            Route::get('/', 'create')->name('create');
+            Route::post('clock-in', 'clockIn')->name('clock-in');
+            Route::patch('clock-out', 'clockOut')->name('clock-out');
+            Route::post('break-start', 'breakStart')->name('break-start');
+            Route::patch('break-end', 'breakEnd')->name('break-end');
+        });
 
-    Route::middleware(['auth', 'admin'])->group(function () {
-        Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-            ->name('logout');
+    Route::controller(AttendanceController::class)
+        ->prefix('attendance')
+        ->name('attendances.')
+        ->group(function () {
+            Route::get('list', 'index')->name('index');
+            Route::get('detail/{attendance}', 'show')
+                ->can('view', 'attendance')
+                ->name('show');
+        });
 
-        Route::get('attendance/list', [AdminAttendanceController::class, 'dailyIndex'])
-            ->name('attendances.daily-index');
-        Route::get(
-            'attendance/staff/{user}',
-            [AdminAttendanceController::class, 'monthlyIndex']
-        )
-            ->name('attendances.monthly-index');
-        Route::get('attendance/{attendance}', [AttendanceController::class, 'show'])
-            ->name('attendances.show');
-        Route::patch(
-            'attendance/{attendance}',
-            [AdminAttendanceController::class, 'update']
-        )
-            ->name('attendances.update');
+    Route::controller(StampCorrectionController::class)
+        ->name('stamp-corrections.')
+        ->group(function () {
+            Route::get('stamp_correction_request/list', 'index')->name('index');
+            Route::post('stamp-correction/{attendance}', 'store')->name('store');
+        });
+});
 
-        Route::get('staff/list', [UserController::class, 'index'])->name('users.index');
+Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
-        Route::get(
-            'attendance/{user}/export',
-            [AdminAttendanceController::class, 'export']
-        )
-            ->name('export');
-    });
+    Route::controller(AdminAttendanceController::class)
+        ->prefix('attendance')
+        ->name('attendances.')
+        ->group(function () {
+            Route::get('list', 'dailyIndex')->name('daily-index');
+            Route::get('staff/{user}', 'monthlyIndex')->name('monthly-index');
+            Route::patch('{attendance}', 'update')->name('update');
+            Route::get('{user}/export', 'export')->name('export');
+        });
+
+    Route::get('attendance/{attendance}', [AttendanceController::class, 'show'])
+        ->name('attendances.show');
+
+    Route::get('staff/list', [UserController::class, 'index'])->name('users.index');
+
+    Route::controller(AdminStampCorrectionController::class)
+        ->prefix('stamp_correction_request/approve/{attendance_correction}')
+        ->name('stamp-corrections.')
+        ->group(function () {
+            Route::get('/', 'show')->name('show');
+            Route::patch('/', 'approve')->name('approve');
+        });
 });
