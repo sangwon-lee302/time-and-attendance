784ae4047aaa1bd84531d4b35adc486412d2e54f
sangwon-lee302 <200340413+sangwon-lee302@users.noreply.github.com>
2026-06-13 19:09:06 +0900

    feat: i dunnno

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,4 +4,10 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase {}
+class ExampleTest extends TestCase
+{
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
