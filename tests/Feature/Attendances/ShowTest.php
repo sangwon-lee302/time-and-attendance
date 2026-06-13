3f769944571fe87e851857cbaabfd61897397235
sangwon-lee302 <200340413+sangwon-lee302@users.noreply.github.com>
2026-06-13 15:31:27 +0900

    feat: implement regular user attendance show test

--- tests/Feature/Attendances/ShowTest.php
+++ tests/Feature/Attendances/ShowTest.php
@@ -5,37 +5,83 @@ namespace Tests\Feature\Attendances;
 use App\Models\Attendance;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Override;
 use Tests\TestCase;
 
 class ShowTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_attendance_show_page_can_be_rendered(): void
+    /**
+     * A mock user instance.
+     * 
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * A mock attendance instance associated with the mock user.
+     * 
+     * @var Attendance
+     */
+    protected $attendance;
+
+    #[Override]
+    function setUp(): void
     {
-        $user       = User::factory()->create();
-        $attendance = Attendance::factory()
-            ->recycle($user)
-            ->hasNonOverlappingBreakTimes(3)
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+        $this->attendance = Attendance::factory()
+            ->recycle($this->user)
+            ->hasNonOverlappingBreakTimes()
             ->create();
+    }
 
+    public function test_attended_user_name_can_be_shown(): void
+    {
+        $this
+            ->actingAs($this->user)
+            ->get('attendance/detail/'.$this->attendance->id)
+            ->assertSeeInOrder([
+                '名前',
+                $this->user->name,    
+            ]);
+    }
+
+    public function test_attended_date_can_be_shown(): void
+    {
+        $this
+            ->actingAs($this->user)
+            ->get('attendance/detail/'.$this->attendance->id)
+            ->assertSeeInOrder([
+                '日付',
+                $this->attendance->date->format('Y年'),    
+                $this->attendance->date->format('n月j日'),    
+            ]);
+    }
+
+    public function test_clock_in_and_out_time_can_be_shown(): void
+    {
+        $this
+            ->actingAs($this->user)
+            ->get('attendance/detail/'.$this->attendance->id)
+            ->assertSeeInOrder([
+                '出勤・退勤',
+                $this->attendance->clocked_in_at->format('H:i'),    
+                $this->attendance->clocked_out_at->format('H:i'),    
+            ]);
+    }
+
+    public function test_breaks_can_be_shown(): void
+    {
         $response = $this
-            ->actingAs($user)
-            ->get('attendance/detail/'.$attendance->id);
+            ->actingAs($this->user)
+            ->get('attendance/detail/'.$this->attendance->id);
 
-        $response->assertOk();
-        $response->assertViewIs('attendances.show');
-        $response->assertSeeInOrder([
-            '名前',
-            '日付',
-            '出勤・退勤',
-            '休憩2',
-            '休憩3',
-            '休憩4',
-            '備考',
-        ]);
-        $response->assertDontSee('休憩1');
-        // check if stamp correction form is enabled
-        $response->assertSee('id="stamp-correction"', false);
+        foreach ($this->attendance->breakTimes as $breakTime) {
+            $response->assertSee($breakTime->started_at->format('H:i'));
+            $response->assertSee($breakTime->ended_at->format('H:i'));
+        }
     }
 }
